0958fee71c51e5572e63749f31f4704fc08b32df
thombruce <thombruce@users.noreply.github.com>
2021-07-25 17:09:16 +0000

    deploy: 7b8673adab77c6bb97bb0c0171b82f6c431f68b5

---
+++ _nuxt/static/1627232950/blog/suicidality-so-far/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627232950",layout:"default",error:a,serverRendered:b,routePath:"\u002Fblog\u002Fsuicidality-so-far",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"99b34ea7"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
---
+++ _nuxt/static/1627232950/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627232950",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"99b34ea7"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
