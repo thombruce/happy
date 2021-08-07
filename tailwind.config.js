330617146a2886c7f1c8f95b4dc09996c5d10b55
Thom Bruce <thom@thombruce.com>
2021-08-07 17:15:23 +0100

    @thombruce/tnt compatibility fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,6 @@ module.exports = {
   theme: {
     // To extend the color palette (though this can also be done by adding additional colors to colors object)
     extend: {
-      colors: {
-        primary: { ...colors.yellow, ...{ DEFAULT: colors.yellow['500'] } },
-        secondary: { ...colors.amber, ...{ DEFAULT: colors.amber['500'] } },
-        dark: { ...colors.warmGray, ...{ DEFAULT: colors.warmGray['900'] } },
-        light: { ...colors.yellow, ...{ DEFAULT: colors.yellow['50'] } }
-      },
       // To add fonts
       fontFamily: {
         poster: ['Life Savers']
