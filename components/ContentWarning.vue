fca2191c55f57a434853aa8c8bb9603c32599ab4
Thom Bruce <thom@thombruce.com>
2021-08-07 17:20:18 +0100

    @thombruce/tnt compatibility fixes for custom alert component

--- components/ContentWarning.vue
+++ components/ContentWarning.vue
@@ -3,16 +3,9 @@
   span.alert-context Content warning!
   span.alert-message
     slot
+.alert.alert-error(role='alert')
+  .flex-1
+    strong.mr-2 Content warning!
+    span.alert-message
+      slot
 </template>
-
-<style lang='postcss' scoped>
-.alert {
-  @apply my-5 p-2 bg-danger-800 items-center text-danger-100 leading-none flex rounded-full;
-  & span.alert-context {
-    @apply flex text-center rounded-full bg-danger-500 uppercase px-2 py-1 text-xs font-bold mr-3;
-  }
-  & span.alert-message {
-    @apply font-semibold mr-2 text-left flex-auto;
-  }
-}
-</style>
